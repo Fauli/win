80d6b916fedfff6d8bfd1346d11811b4891af005
Savino Jossi <savino.jossi@gmail.com>
2014-10-11 01:03:24 +0200

    first mysql connection functions

--- web/win.php
+++ web/win.php
@@ -3,8 +3,12 @@
 /* 
  * Output stuff for win 
  *
+ * basically just the visualization, preparing data for output
+ * main logic is on the server
+ *
  */
 
+$mysqli = mysqli_connect("localhost", "root", "#YOLOswag1337", "win");
 
 // load template and end of main logic
 include "index.html";
@@ -15,6 +19,25 @@ exit;
  */
 
 // get shit from db
+
+function getData($mysqli, $dataset){
+
+  $table = array("google_raw" => "google_raw",
+                 "twitter_raw" => "twitter_raw");
+
+  $query = " SELECT * FROM ".$table[$dataset].
+           " WHERE this = ?
+           ";
+
+  try {
+    $stmt = $mysqli->prepare($query)
+    $mysqli->execute($stmt);
+  } catch (mysqli_sql_exception $e){
+    echo $e->errorMessage();
+  }
+
+
+}
 
 
 ?>

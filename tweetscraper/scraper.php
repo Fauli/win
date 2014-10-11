9e55a84a73f431cf4eb82686c144906d1b8f7746
Patrick <patrick@peakinformatik.com>
2014-10-11 05:24:22 +0200

    updated scraper

--- tweetscraper/scraper.php
+++ tweetscraper/scraper.php
@@ -20,19 +20,15 @@ try {
     exit;
 }
 
-$url = 'http://otter.topsy.com/search.js';
-
 $client = new Artax\Client;
 $scraper = new TweetScraper\Scraper($client);
-
-$date = new DateTime();
-$date->setTimestamp(strtotime('2014-10-02'));
-
-$body = $scraper->scrape($url, $date);
-//$body = file_get_contents('data/testdata.json');
-
 $parser = new TweetScraper\Parser;
-$data = $parser->parse($body);
-
 $persister = new TweetScraper\Persister($pdo);
-$persister->persist('btc', $data);
+
+$from = new DateTime('2014-10-01');
+$to = new DateTime('2014-10-10');
+
+$dateRangeScraper = new TweetScraper\DateRangeScraper($scraper, $parser, $persister);
+$dateRangeScraper->setSleepInSeconds(30);
+$dateRangeScraper->setAmount(1000);
+$dateRangeScraper->scrape($from, $to);

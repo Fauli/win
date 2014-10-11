addef12f38b817ad0621b0c057346452aab164d0
Patrick <patrick@peakinformatik.com>
2014-10-11 20:42:44 +0200

    scraper now takes argv

--- tweetscraper/scraper.php
+++ tweetscraper/scraper.php
@@ -1,6 +1,8 @@
 <?php
-
 set_time_limit(0);
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 require_once 'vendor/autoload.php';
 
@@ -9,6 +11,10 @@ $woops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $woops->register();
 
 $db = json_decode(file_get_contents('../db.json'));
+
+if (!array_key_exists(1, $argv) || !array_key_exists(2, $argv)) {
+    exit('Missing Date Arguments. Call script like this: php scraper.php 2014-09-08 2014-10-10' . PHP_EOL);
+}
 
 $host = $db->host;
 $dbname = $db->dbname;
@@ -27,8 +33,8 @@ $scraper = new TweetScraper\Scraper($client);
 $parser = new TweetScraper\Parser;
 $persister = new TweetScraper\Persister($pdo);
 
-$from = new DateTime('2014-07-28');
-$to = new DateTime('2014-10-10');
+$from = new DateTime($argv[1]);
+$to = new DateTime($argv[2]);
 
 $dateRangeScraper = new TweetScraper\DateRangeScraper($scraper, $parser, $persister);
 $dateRangeScraper->setSleepInSeconds(10);

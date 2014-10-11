1800bed806df28fdb567c4a82916c2ab322af6b8
Fauli <franz.faul@hispeed.ch>
2014-10-11 20:44:09 +0200

    notification support

--- web/public/js/gui-logic.js
+++ web/public/js/gui-logic.js
@@ -1,5 +1,7 @@
 $(function() {
 
+    loadNotifications();
+
     // The chart GUI logic, needs adaption when new Sources come to play
     $('#toDatePicker').datepicker()
         .on('changeDate', function(ev){
@@ -43,3 +45,22 @@ $(function() {
     });
 
 });
+
+function loadNotifications(){
+        //alert('notifications');
+        $('#notificationBody').empty();
+
+        $.getJSON("http://151.236.222.251/notifications/getJsonData", function( data ) {
+                var items = [];
+                $.each( data, function( key, val ) {
+                        //items.push( "<li id='" + key + "'>" + val + "</li>" );
+                        $('#notificationBody').append(
+                                '<a href="#" class="list-group-item">'
+                                +'<i class="fa fa-' + val.Glyph + ' fa-fw"></i>' + val.Message
+                                +'<span class="pull-right text-muted small"><em>'+val.Date+'</em>'
+                                +'</span></a>');
+
+                });
+        });
+
+}
